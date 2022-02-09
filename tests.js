8597f45c553ade67b8202c0ccfed13cf374a974b
Matthew Kauff <matthew.kauff@live.com>
2022-02-09 00:03:31 -0500

    add the refactor test / solution for the intro-to-testing

--- tests.js
+++ tests.js
@@ -21,13 +21,13 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Hello, Jane!" when executed', function() {
-        expect(sayHello()).toBe("Hello, Jane!");
+    it('should return the string "Hello, input!" when executed', function() {
+        expect(sayHello("Matt")).toBe("Hello, " + "Matt" + "!");
     });
-    it('should return the string "Hello, Alex!" when executed', function() {
-        expect(sayHello("Alex")).toBe("Hello, Alex!");
-    });
-    it('should return the string "Hello, Alex!" when executed', function() {
-        expect(sayHello("Pat")).toBe("Hello, Pat!");
-    });
+//    it('should return the string "Hello, Alex!" when executed', function() {
+//        expect(sayHello("Alex")).toBe("Hello, Alex!");
+//    });
+//    it('should return the string "Hello, Alex!" when executed', function() {
+//        expect(sayHello("Pat")).toBe("Hello, Pat!");
+//    });
 });
